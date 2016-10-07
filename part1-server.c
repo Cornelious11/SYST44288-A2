1e721aeac8e9400e8f3bcdbb5d73d9e0876f7aa5
Cornelious11 <hilarysouthey@gmail.com>
2016-10-06 21:18:13 -0400

    Adding Socket Retrieval logic
    initial creation of socket retrieval logic, needs error catching before line 37 to catch erroneous input from a command line argument for the socket to connect to

--- part1-server.c
+++ part1-server.c
@@ -1,3 +1,11 @@
+/*
+Hilary Southey
+991376654
+SYST44288 - Assignment 2 (Part 1)
+
+
+
+*/
 #include<arpa/inet.h>
 #include<errno.h>
 #include<netinet/in.h>
@@ -13,7 +21,28 @@ int main (int argc, char *argv[]){
         exit(EXIT_FAILURE);
     }
     else{
-        
+        int listenfd = 0, connfd = 0;
+        struct sockaddr_in serv_addr;
+        char sendBuff[1025];
+        int numrv;
+
+        listenfd = socket(AF_INET, SOCK_STREAM, 0);
+        puts("socket retrieve success");
+
+        memset(&serv_addr, '0', sizeof(serv_addr));
+        memset(sendBuff, '0', sizeof(sendBuff));
+
+        serv_addr.sin_family = AF_INET;
+        serv_addr.sin_addr.s_addr = hton1(INADDR_ANY);
+        serv_addr.sin_port = htons(argv[1]);
+
+        bind(listenfd, (struct sockaddr*)&serv_addr,sizeof(serv_addr));
+
+        if(listen(listenfd, 10) == -1){
+            fputs("Failed to listen",stderr);
+            exit(EXIT_FAILURE);
+        }
+
         exit(EXIT_SUCCESS);
     }
     exit(EXIT_FAILURE);
